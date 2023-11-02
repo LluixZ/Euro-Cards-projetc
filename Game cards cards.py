d093e6f90d040b8a6f04a9f3d50cc1c9ad82f221
Luixz <142626537+LluixZ@users.noreply.github.com>
2023-11-01 23:16:35 -0300

    Create Game cards cards.py

---
+++ Game cards cards.py
@@ -0,0 +1,66 @@
+cartas = {
+    'Álvarez': {
+        'probabilidade': 15.00,
+        'Imagem_da_carta': 'https://i.imgur.com/rcAEX6c.png'
+    },
+    'Osimhen': {
+        'probabilidade': 5.00,
+        'Imagem_da_carta': 'https://i.imgur.com/ThpAYdW.png'
+    },
+    'Vini Jr': {
+        'probabilidade': 1.50,
+        'Imagem_da_carta': 'https://i.imgur.com/SoMbFWG.png'
+    },
+    'Rodri': {
+        'probabilidade': 2.00,
+        'Imagem_da_carta': 'https://i.imgur.com/vjW20xe.png'
+    },
+    'Modric': {
+        'probabilidade': 10.00,
+        'Imagem_da_carta': 'https://i.imgur.com/8wgAsml.png'
+    },
+    'Bernardo Silva': {
+        'probabilidade': 10.00,
+        'Imagem_da_carta': 'https://i.imgur.com/eWoIIEk.png'
+    },
+    'De Bruyne': {
+        'probabilidade': 0.50,
+        'Imagem_da_carta': 'https://i.imgur.com/joqj3T5.png'
+    },
+    'Mabappé': {
+        'probabilidade': 0.20,
+        'Imagem_da_carta': 'https://i.imgur.com/Zf1kgqt.png'
+    },
+    'Haaland': {
+        'probabilidade': 0.10,
+        'Imagem_da_carta': 'https://i.imgur.com/441UjGB.png'
+    },
+    'Messi': {
+        'probabilidade': 0.01,
+        'Imagem_da_carta': 'https://i.imgur.com/4eBrqPj.png'
+    },
+    'Messi CE': {
+        'probabilidade': 0.001,
+        'Imagem_da_carta': 'https://i.imgur.com/ZXtjYBt.png'
+    },
+    'Francisco': {
+        'probabilidade': 33.679,
+        'Imagem_da_carta': 'https://i.imgur.com/RvIZmJS.png'
+    },
+    'Bochini': {
+        'probabilidade': 15.00,
+        'Imagem_da_carta': 'https://i.imgur.com/lME2eRj.png'
+    },
+    'Spencer': {
+        'probabilidade': 5.00,
+        'Imagem_da_carta': 'https://i.imgur.com/f45PB9v.png'
+    },
+    'Rogério Ceni': {
+        'probabilidade': 2.00,
+        'Imagem_da_carta': 'https://i.imgur.com/vA8PdRy.png'
+    },
+    'Pelé': {
+        'probabilidade': 0.01,
+        'Imagem_da_carta': 'https://i.imgur.com/djYgjhk.png'
+    }
+}
